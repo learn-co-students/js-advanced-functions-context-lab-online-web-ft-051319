3a07a3137a12af0fbd8dc18ebdc67d280700127a
mkonig411blk <margot.konig@blackrock.com>
2019-08-30 16:09:18 -0500

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,62 @@
  for you to use if you need it!
  */
 
+
+function createEmployeeRecord(person) {
+    return {
+        firstName: person[0],
+        familyName: person[1],
+        title: person[2],
+        payPerHour: person[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployees(people) {
+    return people.map((person) => {
+        return createEmployeeRecord(person)
+    })
+}
+
+function createTimeInEvent(timeIn) {
+    let [date, hour] = timeIn.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),date
+    })
+
+    return this
+}
+
+function createTimeOutEvent(timeOut) {
+    let [date, hour] = timeOut.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10), date
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let timeInEvent = this.timeInEvents.find((event) => {
+        return event.date === date
+    })
+
+    let timeOutEvent = this.timeOutEvents.find((event) => {
+        return event.date === date
+    })
+
+    return (timeOutEvent.hour - timeInEvent.hour) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +75,17 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(employeeRecords) {
+    // take all the records and grab the allWagesFor for each record and sum, starting at 0
+    return employeeRecords.reduce(((sum,record) => sum + allWagesFor.call(record)),0)
+}
+
+function createEmployeeRecords(people) {
+    return people.map(person => createEmployeeRecord(person))
+}
+
+function findEmployeebyFirstName(employeeArray, firstName) {
+    return employeeArray.find(person => person.firstName === firstName)
 }
