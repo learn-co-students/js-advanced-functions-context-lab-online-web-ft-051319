2020e0b5f74028949e4715204b4ac6a448f3b832
tanfly <mcewen.stella@gmail.com>
2019-08-31 15:08:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-/* Your Code Here */
+function createEmployeeRecord(array){
+    const employee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return employee; 
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployees(array){
+    return array.map(arr => createEmployeeRecord(arr))
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeInEvent(timeString){
+    let date = timeString.split(" ")[0];
+    let hour = parseInt(timeString.split(" ")[1], 10);
+    this.timeInEvents.push({ "date": date, "hour": hour, "type": "TimeIn"})
+    return this;
+}
+
+function createTimeOutEvent(timeString){
+    let date = timeString.split(" ")[0];
+    let hour = parseInt(timeString.split(" ")[1], 10);
+    this.timeOutEvents.push({"date": date, "hour": hour, "type": "TimeOut"})
+    return this;
+}
+
+function hoursWorkedOnDate(date){
+    let timeIn = this.timeInEvents.find(timeEvent =>{
+        return timeEvent.date === date
+    });
+    let timeOut = this.timeOutEvents.find(timeEvent =>{
+        return timeEvent.date === date
+    });
+
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date)*this.payPerHour
+}
+
+function calculatePayroll(dates){
+    let wages = dates.map(date => allWagesFor.call(date))
+    return wages.reduce((sum, currentValue) => sum + currentValue)
+}
+
+function createEmployeeRecords(data){
+    return data.map(employee => createEmployeeRecord(employee))
+}
+
+function findEmployeebyFirstName(data, firstName){
+    return data.find(employee => employee.firstName === firstName)
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
