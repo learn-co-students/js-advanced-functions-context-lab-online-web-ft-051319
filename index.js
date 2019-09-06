fc4fb2f57990bcacdc04db8c92468dde5700688e
Natalie <natsetc@gmail.com>
2019-09-06 13:46:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-/* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployees(nestedArray) {
+    return nestedArray.map(arr => createEmployeeRecord(arr));
+}
+
+function createTimeInEvent(dateStamp) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(' ')[1], 10),
+        date: dateStamp.split(' ')[0]
+    })
+    return this;
+
+    // other method
+    // let [date, hour] = dateStamp.split(' ')
+}
+
+function createTimeOutEvent(dateStamp) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(' ')[1], 10),
+        date: dateStamp.split(' ')[0]
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let dateTimeIn = this.timeInEvents.find(event => event.date === date);
+    let dateTimeOut = this.timeOutEvents.find(event => event.date === date);
+    return (dateTimeOut.hour - dateTimeIn.hour)/100;
+}
+
+function wagesEarnedOnDate(date) {
+    return (this.payPerHour) * (hoursWorkedOnDate.call(this, date))
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,4 +63,19 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function createEmployeeRecords(nestedArray) {
+    return nestedArray.map(array => createEmployeeRecord(array));
+}
+
+function findEmployeebyFirstName(array, firstName) {
+    return array.find(obj => obj.firstName === firstName);
+    // using .find() bc want return value to be the matching record or undefined. filter() will return an empty array if no matching record found.
+}
+
+function calculatePayroll(array) {
+    return array.reduce( (total, empObj) => {
+        return total + allWagesFor.call(empObj)
+    }, 0)
 }
